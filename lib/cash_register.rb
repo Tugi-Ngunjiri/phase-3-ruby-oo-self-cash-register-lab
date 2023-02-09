d909ab49b8fce17c09eb203823e3fa8b20a55d1d
tugi <blueqwinn@gmail.com>
2023-02-10 00:34:51 +0300

    second commit

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,27 @@
+class Cash_register
+    attr_accessor : Cash
+end
+
+    def initialize(cash = 5000)
+        cash = @cash
+        cash.new = ( 5000 ksh)
+        # => "5000 ksh"
+    end
+
+     def  discount
+        discount.cash = ("100%")
+        # => "100%"
+      end
+    
+
+cash Cash_register
+ attr_accessor : void_last_transaction
+
+ def void_last_transaction
+    void_last_transaction = @void_last_transaction
+    void_last_transaction.new =  "Removed  Void Transaction"
+
+    # => "Removed Void Transaction
+  end
+end
 
