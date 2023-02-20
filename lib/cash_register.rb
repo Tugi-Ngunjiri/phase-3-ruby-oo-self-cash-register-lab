ffb5413fe59840c47a75f62065f6d4d4d42f8c07
tugi <blueqwinn@gmail.com>
2023-02-20 19:30:45 +0300

    Completed Assigment

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,27 +1,42 @@
-class Cash_register
-    attr_accessor : Cash
-end
+class CashRegister
+  attr_accessor :discount, :total
+  attr_reader :items
 
-    def initialize(cash = 5000)
-        @cash = cash
-        cash.new = ( 5000 ksh)
-        # => "5000 ksh"
-    end
+  def initialize(discount = 0)
+    @discount = discount
+    @total = 0
+    @items = []
+    @last_item_price = 0
+  end
 
-     def  discount
-        discount.cash = ("100%")
-        # => "100%"
+  def add_item(item, price, *quant)
+    if quant.count == 0
+      @items << item
+      @last_item_price = price
+      @total += price
+    else
+      for i in 1..quant[0] do
+        @items << item
       end
-    
+      @last_item_price = price* quant[0]
+      @total += price * quant[0]
+    end
+  end
 
-cash Cash_register
- attr_accessor : void_last_transaction
-
- def void_last_transaction
-    @void_last_transaction = void_last_transaction
-    void_last_transaction.new =  "Removed  Void Transaction"
-
-    # => "Removed Void Transaction
+  def apply_discount
+    if discount == 0
+      'There is no discount to apply.'
+    else
+      @total = @total * (100 - discount) / 100
+      "After the discount, the total comes to $#{@total}."
+    end
+  end
+  def void_last_transaction
+    @total -= @last_item_price
   end
 end
-
+Footer
+© 2023 GitHub, Inc.
+Footer navigation
+Terms
+Privacy
